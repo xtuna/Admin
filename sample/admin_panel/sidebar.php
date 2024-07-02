33725bb1f2fb22707d09654342fcf2c913c2352f
xtuna <92073982+xtuna@users.noreply.github.com>
2024-07-03 05:50:47 +0800

    Delete sample/admin_panel directory

--- sample/admin_panel/sidebar.php
+++
@@ -1,9 +0,0 @@
-<!-- Sidebar -->
-<div class="d-flex" id="wrapper">
-    <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="sidebar-heading">Admin Panel</div>
-        <div class="list-group list-group-flush">
-            <a href="#storeSection" class="list-group-item list-group-item-action bg-light">Stores</a>
-            <a href="#userSection" class="list-group-item list-group-item-action bg-light">Users</a>
-        </div>
-    </div>
